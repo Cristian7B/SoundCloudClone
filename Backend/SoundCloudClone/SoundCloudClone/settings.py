3fb7ad98052b857e9b4070fdebc8c6ddcc1aeacf
Niko784 <nicolavp0709@gmail.com>
2025-07-04 12:44:08 -0500

    Creada autenticación y buscar canciones o playlist por nombre, falta prueba

--- Backend/SoundCloudClone/SoundCloudClone/settings.py
+++ Backend/SoundCloudClone/SoundCloudClone/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'apiAutenticacion',
+    'apiBuscar',
     'rest_framework',
     'rest_framework_simplejwt',
 ]
--- Backend/SoundCloudClone/SoundCloudClone/urls.py
+++ Backend/SoundCloudClone/SoundCloudClone/urls.py
@@ -20,4 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/auth/', include('apiAutenticacion.urls')),
+    path('buscar/', include('apiBuscar.urls')),
+
 ]
