8a0c08c1ea33b07252b28afb89dfd2af0215761a
Cristian <criscamcr7@gmail.com>
2025-07-10 12:25:24 -0500

    Cambios mios(?)

--- Backend/SoundCloudClone/SoundCloudClone/settings.py
+++ Backend/SoundCloudClone/SoundCloudClone/settings.py
@@ -45,7 +45,13 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'apiAutenticacion',
     'rest_framework',
+    'rest_framework_simplejwt',
 ]
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
